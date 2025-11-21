a61d481257499650d9736f61b49c37a8a09ca963
Jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2025-11-21 15:36:47 +0300

    Added the Magazine.all class variable and the top_publisher() class method

---
+++ list.py
@@ -0,0 +1,12 @@
+#num=[1,2,3,4,5,6,7,8,9,10]
+
+
+#last_element = num[-1]
+
+
+#squared_numbers = [x**2 for x in num]
+
+#print(f"Last element: {last_element}")
+#print(f"Squared numbers: {squared_number
+
+
